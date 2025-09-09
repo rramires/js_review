9ec73118e76fd149a10898458ca13b750af43eee
rramires <ricardo@flexbr.com>
2025-09-09 16:52:31 -0300

    Adding variables.

---
+++ 01_Variaveis/scripts.js
@@ -0,0 +1,37 @@
+// Declarado sem valor
+var user
+console.log(user)
+
+// Declarado com valor
+var email = 'teste@email.com'
+console.log(email)
+
+// Subestituindo um valor
+email = 'fulano@email.com'
+console.log(email)
+
+// Bonus
+//alert(email)
+
+// Explicar Case Sensitive
+var product = 'celular'
+// é diferente de
+var Product = 'notebook'
+console.log(product, Product)
+
+// Utilizando o let
+let user2 = 'Fulano'
+console.log(user2)
+
+// Alterando o conteúdo
+user2 = 'Sicrano'
+console.log(user2)
+
+// Utilizando o const
+const value1 = 55
+console.log(value1)
+// value1 = 33 // Dá erro, não pode ser alterado o valor
+console.log(value1)
+
+// Nomes de variaves Case Sensitive
+
