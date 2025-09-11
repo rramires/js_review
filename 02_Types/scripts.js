4f26daa7636526edd8c88fd3315187dcf4ffd46c
rramires <ricardo@flexbr.com>
2025-09-11 16:15:39 -0300

    Adding Types.

---
+++ 02_Types/scripts.js
@@ -0,0 +1,76 @@
+// String
+let nome = 'Fulano'
+console.log(nome)
+nome = 10
+console.log(nome)
+
+// Number
+let value = 5
+
+// Boolean
+let isValid = true
+console.log(isValid)
+
+// undefined
+let test
+console.log(test)
+
+// null
+let test2 = null
+console.log(test2)
+
+// Tipos
+let test3 = 'Texto'
+console.log(typeof test3)
+
+test3 = 10
+console.log(typeof test3)
+
+test3 = false
+console.log(typeof test3)
+
+test3 = null
+console.log(typeof test3)
+
+test3 = undefined
+console.log(typeof test3)
+
+// Template literals
+let username = 'MyUser'
+let email = 'user@email.com'
+// contatenando
+let message = "Olá " + username + ", seu email é: " + email
+console.log(message)
+
+// Template string
+message = `Olá ${username}, seu email é: ${email}`
+console.log(message)
+// Executa JS entre as chaves
+console.log(`O resultado da soma é: ${10 + 10}`)
+
+// Number
+console.log(typeof 5,typeof -5, typeof 5.25)
+console.log(5, -5, 5.25)
+
+// NaN not a number
+console.log(12 / 'teste')
+
+// Concatenacao de numero com string
+console.log(5 + '5', typeof (5 + '5'))
+
+// Conversão de tipos
+console.log(5 + '5', 5 + Number('5'))
+
+console.log(5 + 5, 5 + String(5))
+console.log(5 + 5, 5 + (5).toString())
+
+let option = 1
+console.log(option, typeof option)
+
+option = Boolean(option)
+console.log(option, typeof option)
+
+
+
+
+
