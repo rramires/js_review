982fab26483c206916c979855a1533237d2716ae
rramires <ricardo@flexbr.com>
2025-10-15 17:13:58 -0300

    Add value mask.

--- 16_RefundProject/scripts.js
+++ 16_RefundProject/scripts.js
@@ -1,1 +1,21 @@
-console.log('Refund !!!')
+// HTML Elements
+const amountUI = document.getElementById('amount')
+
+// Amount mask
+amountUI.oninput = function(){
+    // Deixa somente números
+    let value = amountUI.value.replace(/\D/g, '')
+
+     value = Number(value) / 100
+
+    amountUI.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value){
+    value = value.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    })
+
+    return value
+}
